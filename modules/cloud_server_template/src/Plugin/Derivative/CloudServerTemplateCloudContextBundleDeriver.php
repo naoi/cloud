4f6ee622b8a23ddf16073a2138e9da343df3611e
Baldwin Louie <baldwinlouie@gmail.com>
2018-03-14 22:12:50 -0700

    Clean up comments

--- modules/cloud_server_template/src/Plugin/Derivative/CloudServerTemplateCloudContextBundleDeriver.php
+++ modules/cloud_server_template/src/Plugin/Derivative/CloudServerTemplateCloudContextBundleDeriver.php
@@ -39,12 +39,8 @@ class CloudServerTemplateCloudContextBundleDeriver extends DeriverBase implement
    * {@inheritdoc}
    */
   public function getDerivativeDefinitions($base_plugin_definition) {
-    // TODO: inject cloud_context via dependency injection
-    //$cloud_contexts = \Drupal::entityTypeManager()->getListBuilder('cloud_context')->load();
     foreach ($this->config_storage->loadMultiple() as $cloud_context => $context) {
-    //foreach ($cloud_contexts as $context) {
       $this->derivatives[$context->id()] = $base_plugin_definition;
-      // supply the cloud_context.
       $this->derivatives[$context->id()]['cloud_context'] = $context->id();
     }
     return $this->derivatives;
