f16eebefe252b6403482de70ba34ba2a303fd729
Baldwin Louie <baldwinlouie@gmail.com>
2018-04-30 09:36:21 -0700

    Updated the Add Key Pair functionality.  Following AWS's documentation and not storing the private key in the database.  https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html
    The key pair is written to a temporary file.  The user gets a prompt to download it once they create a new key pair.  Upon download, the file is deleted.

---
+++ modules/cloud_service_providers/aws_cloud/src/Controller/Ec2/AWSKeyPairController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\aws_cloud\Controller\Ec2;
+
+use Drupal\aws_cloud\Aws\Config\ConfigInterface;
+use Drupal\aws_cloud\Entity\Ec2\KeyPair;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Messenger\Messenger;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class AWSKeyPairController extends ControllerBase {
+
+  /**
+   * ApiController constructor.
+   * @param \Drupal\Core\Messenger\Messenger $messenger
+   *  Messanger Object
+   */
+  public function __construct(Messenger $messenger) {
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * Dependency Injection.
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('messenger')
+    );
+  }
+
+  /**
+   * @param \Drupal\aws_cloud\Aws\Config\ConfigInterface $cloud_context
+   * @param \Drupal\aws_cloud\Entity\Ec2\KeyPair $aws_cloud_key_pair
+   * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+   */
+  public function downloadKey(ConfigInterface $cloud_context, KeyPair $aws_cloud_key_pair) {
+    $file = $aws_cloud_key_pair->getKeyFileLocation();
+    if ($file != FALSE) {
+      $response = new BinaryFileResponse($file, 200, [], FALSE, 'attachment');
+      $response->setContentDisposition('attachment', $aws_cloud_key_pair->key_pair_name() . '.pem');
+      $response->deleteFileAfterSend(TRUE);
+      return $response;
+    }
+    else {
+      // just redirect to keypair listing page.
+      return $this->redirect('view.aws_cloud_key_pairs.page_1', [
+        'cloud_context' => $cloud_context->id(),
+      ]);
+    }
+  }
+
+}
